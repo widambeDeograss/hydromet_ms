743a748ed63a15789eda83124f628c7e8eac5516
Arnoldmosha60 <arnoldmosha60@gmail.com>
2024-01-31 13:28:22 +0300

    shs

--- lib/screens/education_content.dart
+++ lib/screens/education_content.dart
@@ -34,7 +34,40 @@ class _EducationContentState extends State<EducationContent> {
                   borderRadius: BorderRadius.circular(15.0),
                 ),
                 child: const Row(
-                  
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    Image(
+                      image: AssetImage(
+                        'assets/images/heavy_rain',
+                      ),
+                    ),
+                    SizedBox(
+                      width: 10.0,
+                    ),
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          'HEAVY RAINFALL',
+                          style: TextStyle(
+                            fontSize: 20.0,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        SizedBox(
+                          height: 5.0,
+                        ),
+                        Text(
+                          'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla condimentum dui sed libero sagittis, '
+                          'ut placerat diam dapibus. Aenean congue tristique augue eu lobortis. Proin faucibus purus '
+                          'aliquam ',
+                          style: TextStyle(
+                            fontSize: 13.0,
+                          ),
+                        ),
+                      ],
+                    )
+                  ],
                 ),
               ),
             ],
