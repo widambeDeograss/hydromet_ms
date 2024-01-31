af80746b7310da3842b8ca5215a118de20bc03aa
Arnoldmosha60 <arnoldmosha60@gmail.com>
2024-01-31 13:50:30 +0300

    shs

--- lib/screens/education_content.dart
+++ lib/screens/education_content.dart
@@ -26,26 +26,23 @@ class _EducationContentState extends State<EducationContent> {
         ),
         child: SingleChildScrollView(
           child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.stretch,
             children: [
               Card(
                 shape: RoundedRectangleBorder(
                   borderRadius: BorderRadius.circular(15.0),
                 ),
                 child: const Row(
-                  crossAxisAlignment: CrossAxisAlignment.stretch,
                   children: [
                     Image(
                       image: AssetImage(
-                        'assets/images/heavy_rain',
+                        'assets/images/heavy_rain.jpg',
                       ),
                     ),
                     SizedBox(
                       width: 10.0,
                     ),
                     Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
                       children: [
                         Text(
                           'HEAVY RAINFALL',
@@ -70,6 +67,48 @@ class _EducationContentState extends State<EducationContent> {
                   ],
                 ),
               ),
+              const SizedBox(height: 16.0,),
+              Card(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(15.0),
+                ),
+                child: const Row(
+                  children: [
+                    Image(
+                      image: AssetImage(
+                        'assets/images/floods.jpg',
+                      ),
+                    ),
+                    SizedBox(
+                      width: 10.0,
+                    ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      children: [
+                        Text(
+                          'HEAVY RAINFALL',
+                          style: TextStyle(
+                            fontSize: 20.0,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        SizedBox(
+                          height: 5.0,
+                        ),
+                        Text(
+                          'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla condimentum dui sed libero sagittis, '
+                              'ut placerat diam dapibus. Aenean congue tristique augue eu lobortis. Proin faucibus purus '
+                              'aliquam ',
+                          style: TextStyle(
+                            fontSize: 13.0,
+                          ),
+                        ),
+                      ],
+                    )
+                  ],
+                ),
+              ),
+              const SizedBox(height: 16.0,),
             ],
           ),
         ),
